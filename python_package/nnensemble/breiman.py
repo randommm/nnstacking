13be04ffd5d0d238969808a96bc50054c9c62beb
Marco Inacio <m@marcoinacio.com>
2018-06-23 15:24:58 -0300

    include copyright notice

--- python_package/nnensemble/breiman.py
+++ python_package/nnensemble/breiman.py
@@ -1,6 +1,19 @@
-'''
-Breiman Stacking
-'''
+#----------------------------------------------------------------------
+# Copyright 2018 Victor Coscrato <vcoscrato@gmail.com>;
+# Copyright 2018 Marco Inacio <pythonpackages@marcoinacio.com>
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3 of the License.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <http://www.gnu.org/licenses/>.
+#----------------------------------------------------------------------
 
 import numpy as np
 from sklearn.base import BaseEstimator
@@ -8,7 +21,9 @@ from sklearn.model_selection import cross_val_predict
 from scipy.optimize import nnls
 
 class LinearStack(BaseEstimator):
-
+    '''
+    Breiman Stacking
+    '''
     def __init__(self, estimators = None, verbose = 1):
         for prop in dir():
             if prop != "self":
